2f8b22132f00c15d3f08d77813cbbeebcc4ae7d7
njir <njir@naver.com>
2016-07-02 13:17:56 +0900

    feat: Add Tak test

---
+++ app/controllers/TakController.scala
@@ -0,0 +1,24 @@
+/**
+  * Created by tak on 16. 7. 2.
+  */
+
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+@Singleton
+class TakController @Inject() extends Controller {
+
+  /**
+   * Create an Action to render an HTML page with a welcome message.
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def tak = Action {
+    Ok(views.html.index("Tak Test"))
+  }
+
+}
